ff7fdeaccbd817fc3283ac94b570e41863ff1f84
Jojo Ngai <jojo.ngai2003@gmail.com>
2024-02-12 15:45:15 -0500

    Finished lab6 and changed config.js to connect to mySQL db

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
-  host    : 'ec2-3-137-65-169.us-east-2.compute.amazonaws.com',
-  user    : '',
-  password: '',
-  database: ''
+  host: 'ec2-3-137-65-169.us-east-2.compute.amazonaws.com',
+  user: 'j5ngai',
+  password: 'MSCI345',
+  database: 'imdb',
 };
- 
+
 export default config;
