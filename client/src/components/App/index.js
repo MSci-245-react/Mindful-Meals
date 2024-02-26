05b3f71e6038cbf7308ca5d9c4de1f7d86539dc4
Jojo Ngai <jojo.ngai2003@gmail.com>
2024-02-25 21:16:50 -0500

    added homepage

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Review from './Review';
 import SignUp from '../SignUp';
 import SignIn from '../SignIn';
+import HomePage from '../HomePage';
 import NutritionalInformation from '../NutritionalInformation';
 import RecipeFinder from '../RecipeFinder';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <NavigationBar />
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/Signup" element={<SignUp />} />
           <Route path="/RecipeFinder" element={<RecipeFinder />} />
           <Route path="/SignIn" element={<SignIn />} />
