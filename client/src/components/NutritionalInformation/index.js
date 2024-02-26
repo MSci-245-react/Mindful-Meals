0706faf9f178b44e46e73e2ef5024ef5eb1d1001
Jojo Ngai <jojo.ngai2003@gmail.com>
2024-02-25 21:43:26 -0500

    README for project and debugging Nutritional information page

--- client/src/components/NutritionalInformation/index.js
+++ client/src/components/NutritionalInformation/index.js
@@ -1,56 +1,62 @@
 import React, {useEffect, useState} from 'react';
+import './NutritionalInformationTable.css';
 
-function NutritionalInformation() {
-  const [nutritionalInfo, setNutritionalInfo] = useState([]);
+const NutritionalInformationTable = () => {
+  const [nutritionalData, setNutritionalData] = useState([]);
 
   useEffect(() => {
-    // Fetch nutritional information from your API
-    fetch('/api/getNutritionalInfo')
-      .then(response => response.json())
-      .then(data => {
-        // Assuming the backend sends the data directly without a nested structure
-        setNutritionalInfo(data);
+    fetch('/api/getNutritionalInfo') // Update this to match your correct backend endpoint
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
       })
-      .catch(error => console.error('There was an error!', error));
+      .then(data => setNutritionalData(data))
+      .catch(error => {
+        console.error(
+          'There has been a problem with your fetch operation:',
+          error,
+        );
+      });
   }, []);
 
   return (
-    <div>
-      <h1>Nutritional Information</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Calories</th>
-            <th>Fat Content</th>
-            <th>Saturated Fat</th>
-            <th>Cholesterol</th>
-            <th>Sodium</th>
-            <th>Carbohydrates</th>
-            <th>Fiber</th>
-            <th>Sugar</th>
-            <th>Protein</th>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Calories</th>
+          <th>Fat Content</th>
+          <th>Saturated Fat</th>
+          <th>Cholesterol</th>
+          <th>Sodium</th>
+          <th>Carbohydrates</th>
+          <th>Fiber</th>
+          <th>Sugar</th>
+          <th>Protein</th>
+          {/* Add more headers as needed */}
+        </tr>
+      </thead>
+      <tbody>
+        {nutritionalData.map((item, index) => (
+          <tr key={index}>
+            <td>{item.Name}</td>
+            <td>{item.Calories}</td>
+            <td>{item.FatContent}</td>
+            <td>{item.SaturatedFatContent}</td>
+            <td>{item.CholesterolContent}</td>
+            <td>{item.SodiumContent}</td>
+            <td>{item.CarbohydrateContent}</td>
+            <td>{item.FiberContent}</td>
+            <td>{item.SugarContent}</td>
+            <td>{item.ProteinContent}</td>
+            {/* Render more data as needed */}
           </tr>
-        </thead>
-        <tbody>
-          {nutritionalInfo.map((item, index) => (
-            <tr key={index}>
-              <td>{item.Name}</td>
-              <td>{item.Calories}</td>
-              <td>{item.FatContent}</td>
-              <td>{item.SaturatedFatContent}</td>
-              <td>{item.CholesterolContent}</td>
-              <td>{item.SodiumContent}</td>
-              <td>{item.CarbohydrateContent}</td>
-              <td>{item.FiberContent}</td>
-              <td>{item.SugarContent}</td>
-              <td>{item.ProteinContent}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
-}
+};
 
-export default NutritionalInformation;
+export default NutritionalInformationTable;
