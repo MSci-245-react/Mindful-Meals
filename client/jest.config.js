a3526510744258110ed374db61c6a2488f6e248c
Jojo Ngai <jojo.ngai2003@gmail.com>
2024-02-25 22:43:19 -0500

    jest test changes

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    transform: {
-      "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    testEnvironment: 'jsdom',
-  };
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+  },
+};
