70d02bf2144f7dff4f571c6dadf3e6f460de6b5e
Vardhman Jain <vjsiddha@uwaterloo.ca>
2024-03-28 01:22:29 -0400

    Cart updates done

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,51 +2,56 @@ import React, { createContext, useState, useEffect } from 'react';
 
 export const CartContext = createContext();
 
-export const CartProvider = ({ children }) => { 
-  const [cartItems, setCartItems] = useState([]);
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState(() => {
+    // Try to load the cart items from localStorage
+    const savedCartItems = localStorage.getItem('cartItems');
+    return savedCartItems ? JSON.parse(savedCartItems) : [];
+  });
+
+  useEffect(() => {
+    // Save the cart items to localStorage whenever they change
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = (newItem) => {
     setCartItems((prevItems) => {
       const itemIndex = prevItems.findIndex((item) => item.id === newItem.id);
       if (itemIndex > -1) {
+        // Item already exists, update quantity
         const updatedItems = [...prevItems];
         updatedItems[itemIndex].quantity += 1;
         return updatedItems;
       } else {
+        // New item, add to cart
         return [...prevItems, { ...newItem, quantity: 1 }];
       }
     });
   };
 
   const increaseItemQuantity = (itemId) => {
-    console.log('Attempting to increase quantity for item with id:', itemId);
     setCartItems((currentItems) =>
       currentItems.map((item) =>
         item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
       )
     );
   };
-  
+
   const decreaseItemQuantity = (itemId) => {
-    console.log('Attempting to decrease quantity for item with id:', itemId);
     setCartItems((currentItems) =>
       currentItems.map((item) => {
         if (item.id === itemId) {
-          return item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item;
+          const newQuantity = item.quantity - 1;
+          return newQuantity > 0 ? { ...item, quantity: newQuantity } : null;
         }
         return item;
-      }).filter((item) => item.quantity > 0) // This removes the item if the quantity is 0
+      }).filter((item) => item != null)
     );
   };
-  
+
   const removeFromCart = (itemId) => {
-    console.log('Attempting to remove item with id:', itemId);
     setCartItems((currentItems) => currentItems.filter((item) => item.id !== itemId));
   };
-
-  useEffect(() => {
-    console.log('Cart Items Updated:', cartItems);
-  }, [cartItems]);
 
   return (
     <CartContext.Provider value={{
@@ -54,7 +59,7 @@ export const CartProvider = ({ children }) => {
       addToCart,
       removeFromCart,
       increaseItemQuantity,
-      decreaseItemQuantity,
+      decreaseItemQuantity, 
     }}>
       {children}
     </CartContext.Provider>
