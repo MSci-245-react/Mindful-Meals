c349a3fc9f203d02c132e86a68f6ba5f175b33b4
Vardhman Jain <vjsiddha@uwaterloo.ca>
2024-03-28 01:19:05 -0400

    Cart page updates

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from '../HomePage';
 import SignUp from '../SignUp';
 import SignIn from '../SignIn';
-import HomePage from '../HomePage';
 import Profilepage from '../Profilepage'
 import NutritionalInformation from '../NutritionalInformation';
 import RecipeFinder from '../RecipeFinder';
 import RecipeDetail from '../RecipeDetail';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import NavigationBar from '../NavigationBar';
+import CartProvider from '../Cart'; // Using braces for a named export
+import CartPage from '../CartPage'; 
 
 function App() {
   return (
-    <div>
+    <CartProvider> {/* Wrap your application with CartProvider */}
       <Router>
         <NavigationBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/Signup" element={<SignUp />} />
-          <Route path="/RecipeFinder" element={<RecipeFinder />} />
+          <Route path="/SignUp" element={<SignUp />} />
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/Profilepage" element={<Profilepage />} />
-          <Route
-            path="/NutritionalInformation"
-            element={<NutritionalInformation />}
-          />
+          <Route path="/NutritionalInformation" element={<NutritionalInformation />} />
+          <Route path="/RecipeFinder" element={<RecipeFinder />} />
           <Route path="/recipe/:RecipeId" element={<RecipeDetail />} />
+          <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Router>
-    </div>
+    </CartProvider>
   );
 }
 
