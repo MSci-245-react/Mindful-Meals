da8a6d2346455ce2283cbe2ffda12dc33e6f28ef
kirtibansal2002 <kirtibansalva@gmail.com>
2024-04-08 04:08:18 -0400

    fixed jest tests

--- client/src/tests/ingredientAddToArray.test.js
+++ client/src/tests/ingredientAddToArray.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import RecipeFinder from '../components/RecipeFinder/index.js';
+
+jest.mock('../components/Firebase', () => ({
+  withFirebase: Component => props => <Component {...props} firebase={{ auth: { currentUser: { email: 'test@test.ca' } } }} />
+}));
 
 describe('RecipeFinder Component', () => {
   test('checks if inputted ingredients are added to ingredient list', async () => {
@@ -12,7 +16,7 @@ describe('RecipeFinder Component', () => {
 
     await userEvent.type(input, 'onion');
 
-    const addButton = screen.getByRole('button', {name: 'Add'});
+    const addButton = screen.getByRole('button', { name: 'Add' });
     await userEvent.click(addButton);
 
     expect(screen.getByText('onion')).toBeInTheDocument();
--- client/src/tests/nutritionaInformation.test.js
+++ client/src/tests/nutritionaInformation.test.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
+// Import CartContext and any other dependencies
+import { CartContext } from '../components/Cart/index.js';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NutritionalInformationTable from '../components/NutritionalInformation/index.js';
+
+// Mock CartContext
+jest.mock('../path/to/CartContext', () => ({
+  ...jest.requireActual('../path/to/CartContext'), // Use the actual CartContext module
+  useContext: () => ({ addToCart: jest.fn() }), // Mock addToCart function
+}));
+
+// Mock Firebase
+jest.mock('../components/Firebase', () => ({
+  withFirebase: Component => props => (
+    <Component {...props} firebase={{ auth: { currentUser: { email: 'test@test.ca' } } }} />
+  ),
+}));
 
 describe('Nutritional Information Component', () => {
   test('should contain the appropriate headings in the chart', () => {
