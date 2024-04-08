0748d904ace0026639c9cfd8e96a9c6381483aa8
Vardhman Jain <vjsiddha@uwaterloo.ca>
2024-04-08 11:05:42 -0400

    jest test update

--- client/src/tests/profile.test.js
+++ client/src/tests/profile.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import ProfilePage from '../components/Profilepage';
 
 jest.mock('../components/Firebase', () => ({
     withFirebase: Component => props => <Component {...props} firebase={{ auth: { currentUser: { email: 'test@test.ca' } } }} />
 }));
 
-
 describe('ProfilePage Component', () => {
-    test('renders ProfilePage component', () => {
-        const { getByText } = render(<ProfilePage />);
-        const welcomeMessage = getByText(/Welcome to Mindful Meals/i);
+    test('renders ProfilePage component', async () => { // Note the async keyword here
+        const { findByText } = render(<ProfilePage />);
+        const welcomeMessage = await findByText(/Welcome to Mindful Meals/i); // Use findByText to wait for the element to appear
         expect(welcomeMessage).toBeInTheDocument();
     });
 });
