8eb525ae578affa0e50c09b7f0053b63cdf5ab55
Jojo Ngai <jojo.ngai2003@gmail.com>
2024-04-08 12:21:35 -0400

    daily scrums

--- client/src/tests/nutritionaInformation.test.js
+++ client/src/tests/nutritionaInformation.test.js
@@ -1,27 +1,31 @@
-import { CartContext } from '../components/Cart/index.js';
-import { render, screen } from '@testing-library/react';
+import {CartContext} from '../components/Cart/index.js';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NutritionalInformationTable from '../components/NutritionalInformation/index.js';
 
-// Mock Firebase 
+// Mock Firebase
 jest.mock('../components/Firebase', () => ({
-  withFirebase: Component => props => (
-    <Component {...props} firebase={{ auth: { currentUser: { email: 'test@test.ca' } } }} />
-  ),
+  withFirebase: Component => props =>
+    (
+      <Component
+        {...props}
+        firebase={{auth: {currentUser: {email: 'test@test.ca'}}}}
+      />
+    ),
 }));
 
 // Mock context value
 const mockCartContextValue = {
   addToCart: jest.fn(), // Mock function for addToCart
   // Add other context values if there are any
-}; 
+};
 
 describe('Nutritional Information Component', () => {
   test('should contain the appropriate headings in the chart', () => {
     render(
       <CartContext.Provider value={mockCartContextValue}>
         <NutritionalInformationTable />
-      </CartContext.Provider>
+      </CartContext.Provider>,
     );
     expect(screen.getByText('Name')).toBeInTheDocument();
     expect(screen.getByText('Carbohydrates (g)')).toBeInTheDocument();
